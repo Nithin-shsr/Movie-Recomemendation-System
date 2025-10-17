365c2bffe6fe7f65087d1c0d25bd2fb8153dc2ac
Nithin <nithin.shsr@gmail.com>
2025-10-17 16:00:37 +0530

    Implemented Search

--- src/App.jsx
+++ src/App.jsx
@@ -28,11 +28,14 @@ function App() {
 
     const[movieList, setMovieList] = useState([]);
 
-    const fetchMovies = async () => {
+    const fetchMovies = async (query = '') => {
         setIsLoading(true);
         setErrorMessage('');
         try{
-            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const endpoint = query
+                ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+                : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
             const response = await fetch(endpoint, API_OPTIONS);
             if(!response.ok){
                 throw new Error('Failed to fetch movies');
@@ -56,8 +59,8 @@ function App() {
     }
 
     useEffect(()=>{
-        fetchMovies();
-    },[]);
+        fetchMovies(searchTerm);
+    },[searchTerm]);
 
   return (
     <main>
