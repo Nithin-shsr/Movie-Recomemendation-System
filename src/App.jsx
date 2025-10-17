57a283d6b72d4b9a77408e6bcc7527c0f22b9adb
Nithin <nithin.shsr@gmail.com>
2025-10-17 15:19:41 +0530

    TMDB API Integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-// import heroImage from '../public/hero-img.png'
+import {useEffect, useState} from 'react'
 import './App.css'
 import Search from "./Components/SearchBar.jsx";
+import Loader from "./Components/Loader.jsx";
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_KEY}`,
+    }
+}
+
 
 function App() {
 
-    //This useState HOOK is used to manage the actions on the Search bar..
+    //This useState HOOK is used to manage the actions on the Search bar...
     const[searchTerm, setSearchTerm] = useState('');
+
+    const[errorMessage, setErrorMessage] = useState('');
+
+    const[isLoading, setIsLoading] = useState(false);
+
+    const[movieList, setMovieList] = useState([]);
+
+    const fetchMovies = async () => {
+        setIsLoading(true);
+        setErrorMessage('');
+        try{
+            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const response = await fetch(endpoint, API_OPTIONS);
+            if(!response.ok){
+                throw new Error('Failed to fetch movies');
+            }
+            const data = await response.json();
+            if(data.Response==='False')
+            {
+                setErrorMessage(data.Response);
+                setMovieList([]);
+                return;
+            }
+            setMovieList(data.results || []);
+        }
+        catch(error){
+            console.log(`Error Fetching Movies ${error}`);
+            setErrorMessage(data.error || 'Failed to  Fetch Movies');
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(()=>{
+        fetchMovies();
+    },[]);
 
   return (
     <main>
@@ -16,10 +65,24 @@ function App() {
                 <header>
                     <img src="../public/hero-img.png" alt="" />
                     <h1>Find <span className = "text-gradient">Movies</span> You'll Enjoy</h1>
+                    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
                 </header>
-
-                <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-                <h1 className="text-white text-7xl">{searchTerm}</h1>
+                <section className="all-movies">
+                    <h2 className="mt-[20px]">ALl Movies</h2>
+                    {
+                        isLoading ?(
+                            <Loader/>
+                        ) : errorMessage ? (
+                            <p className="text-red-500">{errorMessage}</p>
+                        ):(
+                            <ul>
+                                {movieList.map((movie) => (
+                                    <p key={movie.id} className="text-white">{movie.title}</p>
+                                ))}
+                            </ul>
+                        )
+                    }
+                </section>
             </div>
     </main>
   )
